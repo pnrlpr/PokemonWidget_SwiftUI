53409b3ad04654e78828fa5043c68530c59955c4
pinaralper <ppinaralper@gmail.com>
2023-10-03 13:29:56 +0300

    Initial Commit

---
+++ PokemonWidget_SwiftUI/PokemonWidget_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWidget_SwiftUIApp.swift
+//  PokemonWidget_SwiftUI
+//
+//  Created by PINAR KALKAN on 3.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWidget_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
